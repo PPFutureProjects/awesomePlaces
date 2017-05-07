33fc825706775cbb97e1288ea737fe3f8a769329
pedro azin <pedroazin@gmail.com>
2017-05-07 15:02:11 -0300

    Initial commit

---
+++ src/models/place.ts
@@ -0,0 +1,4 @@
+import { Location } from './lacation';
+export class Place {
+    constructor(public title: string, public description: string, public location: Location, public imagePath: string){}
+}
